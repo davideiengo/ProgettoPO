f832cf09e137b8246b06852114767a17870f869a
Lazarefx1 <antgrind@gmail.com>
2025-06-26 19:18:43 +0200

    TUTTI GLI AGGIORNAMENTI VERRANO PUBBLICATI NEL PUSH FINALE

--- src/main/java/database/DBConnection.java
+++ src/main/java/database/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 public class DBConnection {
     private static final String URL = "jdbc:postgresql://localhost:5432/hackathon_db";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "progetto";
+    private static final String PASSWORD = "Alessandro2";
 
     public static Connection getConnection() throws Exception {
         try {
