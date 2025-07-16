dbda3f45c7a09620d3dd4864ebe7651ce9aa3ecc
Lazarefx1 <antgrind@gmail.com>
2025-07-16 20:02:19 +0200

    Aggiunti commenti per javadoc, spiegazione dettagliata tutte le classi

--- src/main/java/dao/GiudiceDAO.java
+++ src/main/java/dao/GiudiceDAO.java
@@ -5,6 +5,7 @@ import Entity.HackaThon;
 
 import java.util.List;
 
+
 public interface GiudiceDAO {
     void aggiungiGiudice(Giudice giudice, HackaThon hackathon, String password);
     boolean verificaPassword(Giudice giudice, String password);
